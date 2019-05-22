3ff27497ffa678c7bac5c5c83ce8b19dfa56e82e
Víctor Córdova <theslapboy@gmail.com>
2019-05-22 00:13:11 -0700

    Bline

--- app/src/main/java/com/gottnext/bline/HomeFragment.java
+++ app/src/main/java/com/gottnext/bline/HomeFragment.java
@@ -15,4 +15,7 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
+    public void serviceProvider(View view) {
+    }
 }
