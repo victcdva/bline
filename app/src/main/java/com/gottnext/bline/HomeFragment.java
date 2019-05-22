5d25f8cddb6696dbb73876e27d9f567b13dd7ecd
Víctor Córdova <theslapboy@gmail.com>
2019-05-22 10:34:50 -0700

    Bline

--- app/src/main/java/com/gottnext/bline/HomeFragment.java
+++ app/src/main/java/com/gottnext/bline/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.gottnext.bline;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,15 +8,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        ImageView imageView = v.findViewById(R.id.serviceprovider);
+        imageView.setOnClickListener(this);
+        return v;
     }
 
-    public void serviceProvider(View view) {
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.serviceprovider:
+                Intent intent = new Intent(getActivity(), TheHomeActivity.class);
+                getActivity().startActivity(intent);
+                break;
+        }
     }
 }
--- app/src/main/java/com/gottnext/bline/UpcomingFragment.java
+++ app/src/main/java/com/gottnext/bline/UpcomingFragment.java
@@ -1,5 +1,6 @@
 package com.gottnext.bline;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,12 +8,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
-public class UpcomingFragment extends Fragment {
+public class UpcomingFragment extends Fragment implements View.OnClickListener {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_upcoming, container, false);
+        View v = inflater.inflate(R.layout.fragment_upcoming, container, false);
+
+        ImageView imageView = v.findViewById(R.id.enespera);
+        imageView.setOnClickListener(this);
+        return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.enespera:
+                Intent intent = new Intent(getActivity(), UpcomingActivity.class);
+                getActivity().startActivity(intent);
+                break;
+        }
     }
 }
